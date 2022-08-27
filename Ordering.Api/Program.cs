0585db2778b8503ecf9c7be7ec46adfdd0e58233
al.mahmoudi <alireza.mahmoudi.71@gmail.com>
2022-08-27 09:23:32 +0430

    ordercontroller

--- Ordering.Api/Program.cs
+++ Ordering.Api/Program.cs
@@ -1,9 +1,13 @@
+using FluentValidation;
+using MediatR;
 using Ordering.Application.Mappings;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 // Add services to the container.
-
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
