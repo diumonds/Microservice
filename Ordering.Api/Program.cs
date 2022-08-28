13de05ceecfe071ac0ba27e5417ef1bf8f6c25eb
al.mahmoudi <alireza.mahmoudi.71@gmail.com>
2022-08-28 10:17:08 +0430

    applicationlayer

--- Ordering.Api/Program.cs
+++ Ordering.Api/Program.cs
@@ -1,6 +1,13 @@
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
 using Ordering.Application.Mappings;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,9 +19,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("OrderingConnectionString")));
 
+builder.Services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 var app = builder.Build();
-
+builder.Services.Configure<EmailSettings>(c => builder.Configuration.GetSection("EmailSettings"));
+builder.Services.AddTransient<IEmailService, EmailService>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
