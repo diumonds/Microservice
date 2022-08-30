908d5823daccf0e3dc5ab95e6f4c0a5f7e34879e
al.mahmoudi <alireza.mahmoudi.71@gmail.com>
2022-08-30 10:08:57 +0430

    rabbitmq

--- Ordering.Api/Program.cs
+++ Ordering.Api/Program.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Ordering.Application;
 using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Persistence;
 using Ordering.Application.Mappings;
@@ -12,16 +13,14 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAutoMapper(typeof(MappingProfile));
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 // Add services to the container.
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<OrderContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("OrderingConnectionString")));
-
+builder.Services.AddApplicationServices();
 builder.Services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.Configure<EmailSettings>(c => builder.Configuration.GetSection("EmailSettings"));
